8f535fd16d3a9032f5536b8ac6fbb1c60311ea10
wilbert-mhlanga <wilbermhlanga@gmail.com>
2021-04-11 13:23:07 +0200

    Create Joke Service. Closes #3

---
+++ src/main/java/guru/springframework/spring5jokesapp/services/JokeServiceImpl.java
@@ -0,0 +1,24 @@
+package guru.springframework.spring5jokesapp.services;
+
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+
+    private  final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+
+    @Override
+    public String getJoke() {
+
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
